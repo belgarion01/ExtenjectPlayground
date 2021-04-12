91e3b90cfc6b5d85306a8e866df5a30312bd37ff
Hugo RAGUIN <hugo.raguin@gmail.com>
2021-04-12 21:09:03 +0200

    Added base project

---
+++ Assets/Scripts/Asteroid/ShipController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Hura.Asteroid
+{
+    public class ShipController : MonoBehaviour
+    {
+        
+    }
+}
